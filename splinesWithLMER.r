474f3e43572bcfd78bbda2cb477c74f5bec0e4f5
Gustavo de los Campos <gdeloscampos@gmail.com>
2016-02-08 05:23:35 -0500

    update

--- splinesWithLMER.r
+++ splinesWithLMER.r
@@ -13,14 +13,16 @@ rm(list=ls())
  outputFolder='~/Dropbox/ARVALIS/SPLINES/output'
  covNum<-NULL # by default it runs all covariates, otherwise specify a vector in integers
  plotLines<-FALSE
+ nRecordsPrint<-100
 ###
-
+ 
+ 
  dir.create(outputFolder)
  setwd(outputFolder)
  load(phenotypeFile)
  load(envCovFile)
  
- tmp<-names(table(Y$VAR))[table(Y$VAR)>50]
+ tmp<-names(table(Y$VAR))[table(Y$VAR)>100]
  W<-W[Y$VAR%in%tmp,]
  Y<-Y[Y$VAR%in%tmp,]
 
@@ -29,12 +31,15 @@ rm(list=ls())
  LOCxYEAR=paste(Y$LOC,Y$YEAR,sep='-')
  VAR=Y$VAR
  
- 
+  counts<-table(Y$VAR)
+ IDs<-names(counts)[which(counts>nRecordsPrint)]
+
  covNames<-colnames(W)
+ pdf('plots.pdf')
  fileOut<-file('logLik_varE.txt',open='w') 
  msg0<-c( c('ll_Int','ll_LiF' ,'ll_SpF' ,'ll_LiR' ,'ll_SpR'),
- 		 c('var_Int','var_LiF','var_SpF','var_LiR','var_SpR'),
- 		 c('BIC_Int','BIC_LiF','BIC_SpF','BIC_LiR','BIC_SpR')
+ 		 c('BIC_LiF','BIC_SpF','BIC_LiR','BIC_SpR'),
+ 		 'varExp'
  	)
  write(file=fileOut,append=T,x=msg0,ncol=length(msg0))
  if(is.null(covNum)){ covNum=1:ncol(W) } 
@@ -49,13 +54,14 @@ rm(list=ls())
   ec<-ec+runif(min=-sd(ec)/1000,max=sd(ec)/1000,n=length(ec))
   EC.ns=ns(ec,df=4)
   
+  varX=apply(MARGIN=2,X=EC.ns,FUN=var)  
   x1=EC.ns[,1]
   x2=EC.ns[,2]
   x3=EC.ns[,3]
   x4=EC.ns[,4]
 
   PC.W<-svd(W[,-i],nv=0,nu=nPC)$u
-
+  PC.W<-scale(PC.W,scale=F,center=T)
   fmInt <-lmer(y~(1|VAR)+(1|LOCxYEAR)+PC.W,REML=F)
  
   fmLiF <-lmer(y~(1|VAR)+(1|LOCxYEAR)+PC.W+ec,REML=F)
@@ -64,19 +70,74 @@ rm(list=ls())
   fmLiR <-lmer(y~(1|VAR)+(1|LOCxYEAR)+PC.W+ec+(ec-1|VAR),REML=F)
   fmSpR<-lmer( y~(1|VAR)+(1|LOCxYEAR)+PC.W+x1+x2+x3+x4+(x1-1|VAR)+(x2-1|VAR)+(x3-1|VAR)+(x4-1|VAR),REML=F )
   
-
+  varB<-as.data.frame(VarCorr(fmSpR))[as.data.frame(VarCorr(fmSpR))[,2]%in%c('x1','x2','x3','x4'),4][4:1]
+  varExp<-crossprod(varX,varB)
+  BICInt<-BIC(fmInt)
   msg<-c( logLik(fmInt),logLik(fmLiF),logLik(fmSpF),logLik(fmLiR),logLik(fmSpR),
-  	      summary(fmInt)$sigma^2,summary(fmLiF)$sigma^2,summary(fmSpF)$sigma^2,summary(fmLiR)$sigma^2,summary(fmSpR)$sigma^2,
-  	      BIC(fmInt),BIC(fmLiF),BIC(fmSpF),BIC(fmLiR),BIC(fmSpR)
+  	      BICInt-BIC(fmLiF),BICInt-BIC(fmSpF),BICInt-BIC(fmLiR),BICInt-BIC(fmSpR),
+  	      varExp
   	    )
   names(msg)=msg0
   print(round(msg,2))      
   
   write(file=fileOut,append=T,x=msg,ncol=length(msg))
-  
-}
 
+  BHat=as.matrix((coef(fmSpR)$VAR[,c('(Intercept)','x1','x2','x3','x4')]))
+  YHat<-matrix(nrow=length(ec),ncol=nrow(BHat),0)
+  
+  for(j in 1:ncol(YHat)){
+    YHat[,j]<-cbind(1,EC.ns)%*%BHat[j,]
+  }
+  
+  plot(numeric()~numeric(),xlim=range(ec),ylim=range(YHat),xlab=covName,ylab='Predicted Yield',
+      main=paste0('Lines with more than ',nRecordsPrint,' records.'))
+ 	abline(v=ec,col=8,lty=1,lwd=.01)
+ 	for(i in 1:length(IDs)){
+    	newLine<-which(rownames(BHat)==IDs[i])
+    	lines(x=sort(ec),y=YHat[order(ec),newLine],col=2,lwd=.5)   
+ 	}
+ 	lines(x=sort(ec),y=rowMeans(YHat)[order(ec)],col=4,lwd=2)
+}
+dev.off()
 close(fileOut)
+
+
+
+
+
+
+
+  # Plots 
+  
+    BHatInt<-as.matrix(coef(fmInt)$VAR)[,1]
+  BHatLinear<-as.matrix(coef(fmLinear)$VAR)[,2:1]
+  BHatSpline<-as.matrix(coef(fmSpline)$VAR)[,1:5]
+
+  YHatInt<-matrix(nrow=length(ec),ncol=nrow(BHatLinear),0)
+  YHatLinear<-YHatInt
+  YHatSpline<-YHatInt
+  
+  
+  for(i in 1:ncol(YHatInt)){
+    YHatSpline[,i]<-cbind(1,EC.ns)%*%BHatSpline[i,]
+  }
+  
+
+  if(plotLines){
+
+		plot(numeric()~numeric(),xlim=range(ec),ylim=range(YHat),xlab=covName,ylab='Predicted Yield',
+      main=paste0('Lines with more than ',nRecordsPrint,' records.'))
+ 	abline(v=ec,col=8,lty=1,lwd=.01)
+ 	for(i in 1:length(IDs)){
+    	newLine<-which(rownames(BHat)==IDs[i])
+    	lines(x=sort(ec),y=YHat[order(ec),newLine],col=2,lwd=.5)   
+ 	}
+ 	lines(x=sort(ec),y=rowMeans(YHat)[order(ec)],col=4,lwd=2)
+ 	
+   }
+   # end plot NS
+   
+
 
 
 TMP=read.table('logLik_varE.txt',header=T,stringsAsFactors=F)
@@ -196,14 +257,7 @@ if(FALSE){
  	lines(x=sort(ec),y=yHat0[order(ec)],lwd=2,col=4)
  	counts<-table(Y$VAR)
  
- 	IDs<-names(counts)[which(counts>nRecordsPrint)]
- 	plot(numeric()~numeric(),xlim=range(ec),ylim=range(YHat),xlab=covName,ylab='Predicted Yield',
-      main=paste0('Lines with more than ',nRecordsPrint,' records.'))
- 	abline(v=ec,col=8,lty=1,lwd=.01)
- 	for(i in 1:length(IDs)){
-    	newLine<-which(rownames(BHat)==IDs[i])
-    	lines(x=sort(ec),y=YHat[order(ec),newLine],col=2,lwd=.5)   
- 	}
+ 
  	lines(x=sort(ec),y=yHat0[order(ec)],lwd=2,col=4)
    }
    # end plot NS
